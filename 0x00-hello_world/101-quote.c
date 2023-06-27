6a171c212b250c541e86cf939215bd229769abfb
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-27 03:17:13 -0700

    print to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 /**
  * main - Entry point
  *
@@ -10,7 +11,7 @@ int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, 1, sizeof(str), stderr);
+	write(2, str, sizeof(str));
 
 	return (1);
 }
