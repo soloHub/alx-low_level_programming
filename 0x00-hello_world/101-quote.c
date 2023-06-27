818be32adf3512516c23ca2db80cae121dbb79b3
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-26 23:52:45 -0700

    print to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints to strerr
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
