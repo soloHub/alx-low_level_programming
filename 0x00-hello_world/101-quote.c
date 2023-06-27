47175121145f8394e998a24143545dec9f08524a
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-27 03:08:10 -0700

    print to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,9 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, 1, sizeof(str), stderr);
 
 	return (1);
 }
