f6ba3c010c1ab1346964f46ae452f0e5268292ea
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-27 02:46:33 -0700

    print to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include <unistd.h>
 /**
  * main - Entry point
  *
@@ -9,8 +8,7 @@
  */
 int main(void)
 {
-	char c[70] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	fwrite(c, 70, 1, stderr);
 	return (1);
 }
