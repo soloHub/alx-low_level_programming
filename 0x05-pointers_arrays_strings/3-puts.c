c5bcc0d9d1ddf1d136767064d382719a80b9c069
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-27 12:41:03 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include<unistd.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	write(stdout, str, _strlen(str));
+}
