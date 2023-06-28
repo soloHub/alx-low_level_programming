098fceb95ece1e16185b4198873bbe97372f4ffc
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-27 22:11:43 -0700

    print half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = _strlen(str) / 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
