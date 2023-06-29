71a37f2971cdd66a392313d9bedbae0277d4aff1
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-29 10:27:52 -0700

    add function definitation

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -15,5 +15,7 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+int isSeperator(char a);
+int isLower(char a);
 
 #endif
