99c19e806aad4c499d327e9ed1fa424dbde92cf0
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-29 03:43:41 -0700

    print a str in rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,10 +9,7 @@ void print_rev(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-
-	}
+	i = _strlen(s) - 1;
 
 	while (i > -1)
 	{
@@ -21,4 +18,20 @@ void print_rev(char *s)
 	}
 
 	_putchar('\n');
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: string
+* Return: length
+*/
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+
+	return (i);
 }
