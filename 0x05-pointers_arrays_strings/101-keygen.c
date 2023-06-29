49e4d52c2b3775f6fa490e0c43c5a619ee2a25b3
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-29 02:56:50 -0700

    generate random pwd

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/**
- * main - generates keygen.
- * Return: 0 Always.
- */
 int main(void)
 {
+	int myrand;
+	int count;
+	int total;
+
+	srand(time(NULL));
+	for (count = 0, total = 272; total > 27; count++)
+	{
+		myrand = (rand() % 27) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
 	return (0);
 }
