ff3a82f6487add6fa3642d7e6871e05d12e96deb
Solomon Kareem <solomon.kareem@gmail.com>
2023-06-30 10:03:52 -0700

    print n t0 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print from @n to 98
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if  (i != 98)
+			{
+				printf(", ");
+			}
+			else
+				printf("\n");
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if  (i != 98)
+			{
+				printf(", ");
+			}
+			else
+				printf("\n");
+		}
+	}
+}
