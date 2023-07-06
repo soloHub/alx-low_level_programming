c7cfe3999e25516a0edea5fb53ff4ab57d2a0b34
Solomon Kareem <solomon.kareem@gmail.com>
2023-07-06 05:16:54 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string @s in reverse
+ * @s: char pointer to string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
