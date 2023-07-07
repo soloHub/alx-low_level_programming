5a9a03180a42ebfcc2b81fb91681ac6f2711c55c
Solomon Kareem <solomon.kareem@gmail.com>
2023-07-06 21:01:47 -0700

    display sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,22 +5,31 @@
  * @n: number to find square root
  * Return: -1 if @n does not have natural roots or @n^0.5
  */
-int _sqrt_rec(int n, int i, int j);
 
-int _sqrt_recursion(int n){
-    int i = 1;
+int _sqrt_recursion(int n)
+{
+	int i = 1;
 	int j = 0;
-    return (_sqrt_rec(n, i, j));
+
+	return (_sqrt_rec(n, i, j));
 }
+
+/**
+ * _sqrt_rec - prints natural square root of @n
+ * @n: number to find square root
+ * @i: index of odd numbers
+ * @j: square counter
+ * Return: -1 if @n does not have natural roots or @j
+ */
 
 int _sqrt_rec(int n, int i, int j)
 {
-	if(n == 0)
+	if (n == 0)
 		return (j);
 	else if (n < 0)
 		return (-1);
 	n = n - i;
 	j++;
 	i = i + 2;
-    return (_sqrt_rec(n, i, j));
+	return (_sqrt_rec(n, i, j));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,4 +12,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
+int _sqrt_rec(int n, int i, int j);
+
 #endif
