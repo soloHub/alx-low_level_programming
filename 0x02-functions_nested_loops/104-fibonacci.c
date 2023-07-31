15d987ce8342c0e1ddbf74c75c0d255d5b749f6b
Solomon Kareem <solomon.kareem@gmail.com>
2023-07-31 08:10:39 -0700

    initial commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,42 +9,32 @@
 int main(void)
 {
 	int i;
-	unsigned long f1 = 0, f2 = 1, sum;
-	unsigned long f1_h1, f1_h2, f2_h1, f2_h2;
-	unsigned long h1, h2;
+	unsigned long int f1 = 0, b1 = 1, f2 = 0, b2 = 2;
+	unsigned long int h1, h2, h3;
 
-	for (i = 0; i < 92; i++)
+	printf("%lu, %lu, ", b1, b2);
+	for (i = 2; i < 98; i++)
 	{
-		sum = f1 + f2;
-		printf("%lu, ", sum);
-
-		f1 = f2;
-		f2 = sum;
-	}
-
-	f1_h1 = f1 / 10000000000;
-	f2_h1 = f2 / 10000000000;
-	f1_h2 = f1 % 10000000000;
-	f2_h2 = f2 % 10000000000;
-
-	for (i = 92; i < 99; i++)
-	{
-		h1 = f1_h1 + f2_h1;
-		h2 = f1_h2 + f2_h2;
-		if (f1_h2 + f2_h2 > 9999999999)
+		if (b1 + b2 > 10000000000 || f2 > 0 || f1 > 0)
 		{
-			h1 += 1;
-			h2 %= 10000000000;
+			h1 = (b1 + b2) / 10000000000;
+			h2 = (b1 + b2) % 10000000000;
+			h3 = f1 + f2 + h1;
+			f1 = f2;
+			f2 = h3;
+			b1 = b2;
+			b2 = h2;
+			printf("%lu%010lu", f2, b2);
 		}
-
-		printf("%lu%lu", h1, h2);
-		if (i != 98)
+		else
+		{
+			h2 = b1 + b2;
+			b1 = b2;
+			b2 = h2;
+			printf("%lu", b2);
+		}
+		if (count != 97)
 			printf(", ");
-
-		f1_h1 = f2_h1;
-		f1_h2 = f2_h2;
-		f2_h1 = h1;
-		f2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
