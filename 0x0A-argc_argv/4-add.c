e83ff03c4b17a3c98670a8e19dc7da0b45571426
Solomon Kareem <solomon.kareem@gmail.com>
2023-08-04 20:21:23 -0700

    print addition of all argument

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	int x, num, total = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (x = 1; argv[x]; x++)
+	{
+		num = strtol(argv[x], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			total += num;
+		}
+	}
+	printf("%d\n", total);
+
+	return (0);
+}
