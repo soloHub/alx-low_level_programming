b824abc485f1175b09984bf3cb423a06e3b8c28a
Solomon Kareem <solomon.kareem@gmail.com>
2023-09-28 21:35:48 -0700

    get squareroot with recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - prints natural square root of @n
+ * @n: number to find square root
+ * Return: -1 if @n does not have natural roots or @n^0.5
+ */
+
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+	int j = 0;
+
+	return (_sqrt_rec(n, i, j));
+}
+
+/**
+ * _sqrt_rec - prints natural square root of @n
+ * @n: number to find square root
+ * @i: index of odd numbers
+ * @j: square counter
+ * Return: -1 if @n does not have natural roots or @j
+ */
+
+int _sqrt_rec(int n, int i, int j)
+{
+	if (n == 0)
+		return (j);
+	else if (n < 0)
+		return (-1);
+	n = n - i;
+	j++;
+	i = i + 2;
+	return (_sqrt_rec(n, i, j));
+}
+
